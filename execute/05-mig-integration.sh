42d53e4529bee4ab80f545b6efce6931bfebd184
Vinil Vadakkepurakkal <82436391+vinil-v@users.noreply.github.com>
2024-06-27 14:19:49 +0530

    3rd commit

--- execute/05-mig-integration.sh
+++ execute/05-mig-integration.sh
@@ -3,5 +3,5 @@ git clone https://gitlab.com/nvidia/hpc/slurm-mig-discovery.git
 cd slurm-mig-discovery/
 gcc -g -o mig -I/usr/local/cuda/include -I/usr/cuda/include mig.c -lnvidia-ml
 ./mig
-cp gres.conf /sched/gres.conf
+echo y | cp gres.conf /sched/gres.conf
 cp cgroup_allowed_devices_file.conf /sched/cgroup_allowed_devices_file.conf
